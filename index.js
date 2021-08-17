fc75c7ce390f70427bebf2ed63d646b105cb643e
LukeSchmude <schmude2002@gmail.com>
2021-08-17 12:53:54 -0500

    first push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { fifaData } = require('./fifa.js')
 
+
 // ⚽️ M  V P ⚽️ //
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
